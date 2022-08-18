c34c4107dbe43f3f019519773ee34e3fd6b52eda
Gibooon <61015424+Gibooon@users.noreply.github.com>
2022-08-18 11:22:53 +0200

    Update waluty.js
    Rozpoczecie uzywania AP exchangerates po zalozeniu nowego konta

--- waluty.js
+++ waluty.js
@@ -8,13 +8,35 @@ var actualDate = year+"-"+month+"-"+day;
 alert("Today is "+actualDate);
 */
 
+
+
 //APIKEY
-var apikey = "bo0HiDbOS3ydGVDzvzuOWw0oITMF6DVG";
+var apikey = "R9PflQ51h9OMZlrgXaGARqxw33wRaCd3";
 //zmienna przetrzymujaca kurs waluty
+//aktualny kurs
 var kursE;
 var kursU;
 var kursG;
 var kursC;
+
+//kurs sprzed 1 dnia
+var kurs1E;
+var kurs1U;
+var kurs1G;
+var kurs1C;
+
+//kurs sprzed tygodnia
+var kurs7E;
+var kurs7U;
+var kurs7G;
+var kurs7C;
+
+
+//kurs sprzed miesiaca
+var kurs3E;
+var kurs3U;
+var kurs3G;
+var kurs3C;
 
 //funkcja sprawdzajaca tendencje danego kursu
 function tendency(){
@@ -24,6 +46,8 @@ function tendencyOff(){
   document.getElementsByClassName("column").style.backgroundColor = "#94aac0";
   }
 
+
+//Sprawdzanie kursów poszczególnych walut za pomocą obiektu JS XMLHttpRequest
 /*
 //kurs EUR
 const EURxhttp = new XMLHttpRequest();
@@ -31,7 +55,7 @@ EURxhttp.onload = function() {
   var final = this.responseText;
   console.log("kurs euro: "+final.slice(171,184));
   kursE = final.slice(171,184);
-  document.getElementById("eur").innerHTML += kursE+" PLN<br>";
+  document.getElementById("e0").innerHTML += kursE+" PLN<br>";
 }
 EURxhttp.open("GET", "http://api.apilayer.com/exchangerates_data/convert?to=EUR&from=PLN&amount=1&apikey="+apikey);
 EURxhttp.send();
@@ -41,8 +65,8 @@ USDxhttp.onload = function() {
   var final = this.responseText;
   console.log("kurs dolara: "+final.slice(171,184));
   kursU = final.slice(171,184);
-  document.getElementById("usd").innerHTML += kursU+" PLN<br>";
-  
+  document.getElementById("u0").innerHTML += kursU+" PLN<br>";
+
 }
 USDxhttp.open("GET", "http://api.apilayer.com/exchangerates_data/convert?to=USD&from=PLN&amount=1&apikey="+apikey);
 USDxhttp.send();
@@ -53,7 +77,7 @@ GBPxhttp.onload = function() {
   var final = this.responseText;
   console.log("kurs funta: "+final.slice(171,184));
   kursG = final.slice(171,184);
-  document.getElementById("gbp").innerHTML += kursG+" PLN<br>";
+  document.getElementById("g0").innerHTML += kursG+" PLN<br>";
   
 }
 GBPxhttp.open("GET", "http://api.apilayer.com/exchangerates_data/convert?to=GBP&from=PLN&amount=1&apikey="+apikey);
@@ -65,10 +89,10 @@ CHFxhttp.onload = function() {
   var final = this.responseText;
   console.log("kurs franka: "+final.slice(171,184));
   kursC = final.slice(171,184);
-  document.getElementById("chf").innerHTML += kursC+" PLN<br>";
+  document.getElementById("c0").innerHTML += kursC+" PLN<br>";
   
 }
 CHFxhttp.open("GET", "http://api.apilayer.com/exchangerates_data/convert?to=CHF&from=PLN&amount=1&apikey="+apikey);
 CHFxhttp.send();
-
-  //.then(result => document.getElementById("three").innerHTML += result)*/
+*/
+  //.then(result => document.getElementById("three").innerHTML += result)
