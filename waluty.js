40794c15eba96e9de7ed689df9be42263bf202de
Gibooon <61015424+Gibooon@users.noreply.github.com>
2022-08-18 12:22:00 +0200

    Update waluty.js
    Ustawilem daty -1d, -7d, -30d potrzebne do sprawdzania kursu sprzed 1 dnia, 7 dni oraz 30 dni

--- waluty.js
+++ waluty.js
@@ -1,4 +1,7 @@
-/*//aktualna data (potrzebna do sprawdzenia kursu sprzed x dni)
+//zmienna zapasowa do -1, -7, -30 date
+var changedDay;
+
+//aktualna data potrzebna do aktualnego kursu
 var date = new Date();
 var day = date.getDate();
 var month = +date.getMonth()+1;
@@ -6,7 +9,110 @@ if(month < 10){month ="0"+month}
 var year = date.getFullYear();
 var actualDate = year+"-"+month+"-"+day;
 alert("Today is "+actualDate);
-*/
+
+//data sprzed jednego dnia
+var date1 = new Date();
+var day1 = date1.getDate()-1;
+var month1 = +date1.getMonth()+1;
+if(month1 < 10){month1 ="0"+month1}
+var year1 = date1.getFullYear();
+if(day1 < 0){
+  month1 -=1
+  if(month1 == -1 || month1 == 1 || month1 == 3 || month1 == 5 || month1 == 7 || month1 == 8 || month1 == 10 || month1 == 12){
+    changedDay = 31;
+    changedDay +=day1;
+    day1 = changedDay;
+  }
+  else if(month1 == 4 || month1 == 6 || month1 == 9 || month1 == 11){
+    changedDay = 30;
+    changedDay +=day1;
+    day1 = changedDay;
+  }
+  else if(month1 == 2){
+    if(year1%4==0 && year1%100==0 && year1%400){
+      changedDay = 29;
+      changedDay += day1;
+      day1 = changedDay;
+    }
+    else if(year1%4==0 && year1%100==0 && year1%400!=0 || year1%4!=0){
+      changedDay = 28;
+      changedDay += day1
+      day1 = changedDay
+    }
+  }
+}
+var date1 = year1+"-"+month1+"-"+day1;
+alert("Yesterday was "+date1);
+
+//data sprzed 7 dniu
+var date7 = new Date();
+var day7 = date7.getDate()-7;
+var month7 = +date7.getMonth()+1;
+if(month7 < 10){month7 ="0"+month7}
+var year7 = date7.getFullYear();
+if(day7 < 0){
+  month7 -=1
+  if(month7 == -1 || month7 == 1 || month7 == 3 || month7 == 5 || month7 == 7 || month7 == 8 || month7 == 10 || month7 == 12){
+    changedDay = 31;
+    changedDay +=day7;
+    day7 = changedDay;
+  }
+  else if(month7 == 4 || month7 == 6 || month7 == 9 || month7 == 11){
+    changedDay = 30;
+    changedDay +=day7;
+    day7 = changedDay;
+  }
+  else if(month7 == 2){
+    if(year7%4==0 && year7%100==0 && year7%400){
+      changedDay = 29;
+      changedDay += day7;
+      day7 = changedDay;
+    }
+    else if(year7%4==0 && year7%100==0 && year7%400!=0 || year7%4!=0){
+      changedDay = 28;
+      changedDay += day7
+      day7 = changedDay
+    }
+  }
+}
+var date7 = year7+"-"+month7+"-"+day7;
+alert("A week ago was "+date7);
+
+//data sprzed 30 dni
+var date3 = new Date();
+var day3 = date3.getDate()-30;
+var month3 = +date3.getMonth()+1;
+if(month3 < 10){month3 ="0"+month3}
+var year3 = date3.getFullYear();
+if(day3 < 0){
+  month3 -=1
+  if(month3 == -1 || month3 == 1 || month3 == 3 || month3 == 5 || month3 == 7 || month3 == 8 || month3 == 10 || month3 == 12){
+    changedDay = 31;
+    changedDay +=day3;
+    day3 = changedDay;
+  }
+  else if(month3 == 4 || month3 == 6 || month3 == 9 || month3 == 11){
+    changedDay = 30;
+    changedDay +=day3;
+    day3 = changedDay;
+  }
+  else if(month3 == 2){
+    if(year3%4==0 && year3%100==0 && year3%400){
+      changedDay = 29;
+      changedDay += day3;
+      day3 = changedDay;
+    }
+    else if(year3%4==0 && year3%100==0 && year3%400!=0 || year3%4!=0){
+      changedDay = 28;
+      changedDay += day3
+      day3 = changedDay
+    }
+  }
+}
+var date3 = year3+"-"+month3+"-"+day3;
+alert("30 days ago was "+date3);
+
+
 
 
 
@@ -47,7 +153,7 @@ function tendencyOff(){
   }
 
 
-//Sprawdzanie kursów poszczególnych walut za pomocą obiektu JS XMLHttpRequest
+//Sprawdzanie aktualnego kursu poszczególnych walut za pomocą obiektu JS XMLHttpRequest
 /*
 //kurs EUR
 const EURxhttp = new XMLHttpRequest();
